32fffae782426910e12542c1a4941e9770d1f6c7
Herick Portugues <herick.portugues@gmail.com>
2020-09-10 16:54:43 -0300

    Get excel file of all available years in df crawler.
    Co-authored-by: Lucas Boaventura <lucasxboaventura@hotmail.com>

--- src/crawlers/df/df/spiders/crimesDF.py
+++ src/crawlers/df/df/spiders/crimesDF.py
@@ -15,9 +15,20 @@ class CrimesDF(scrapy.Spider):
             '/html/body/div[8]/div/div/div/div[2]/table[2]/tbody//tr')
         
         for i, city in enumerate(crime_table):
-           
             if (i > 1):
                 city_name = city.xpath('./td[1]/strong/text()').get().replace('/', " ")
-                city_url_2020 = city.xpath('./td[20]/a/@href').get()
 
-                urllib.request.urlretrieve(str(city_url_2020), f'./excel/{city_name}.xlsx')
+                # Get a list of all tds of the row (collumns)
+                td_list = city.xpath('.//td')
+
+                # Initial year of the data
+                year = 2018
+
+                for j in range(16, len(td_list) + 1, 2):
+                    data_url = city.xpath(f'./td[{j}]/a/@href').get()
+
+                    if(data_url is not None):
+                        # Download and save the excel table
+                        urllib.request.urlretrieve(str(data_url), f'./data/{str(year)}/{city_name}.xlsx')
+
+                    year += 1

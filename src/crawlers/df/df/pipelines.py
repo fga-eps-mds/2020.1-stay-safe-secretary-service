491c6e2a3173a2b9799c15767468287f88294b5d
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-09-11 20:58:46 -0300

    Prepare the datas to save on database
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>

--- src/crawlers/df/df/pipelines.py
+++ src/crawlers/df/df/pipelines.py
@@ -1,6 +1,6 @@
 from itemadapter import ItemAdapter
-import pandas as pd
-import xlrd
+from utils.treat_dataDF import load_data
+import datetime
 
 # db_data = {
 #     "_id": 1,
@@ -13,53 +13,26 @@ import xlrd
 # }
 
 class DfPipeline:
-    db_data = {}
+    data = []
 
     def process_item(self, item, spider):
         return item
 
 
     def close_spider(self, spider):
-        self.db_data["cities"] = load_data(spider.cities)
+        for year in spider.data['years']:
+            db_data = {}
+            db_data["cities"] = load_data(spider.data['cities'], year)
+            db_data['period'] = {
+                'year': year
+            }
 
+            date = datetime.datetime.now()
 
+            db_data['capture_data'] = f'{date.strftime("%d")}/{date.strftime("%m")}/{date.strftime("%Y")}'
 
-cities_data = []
-crimes_nature = ('LATROCÍNIO', 'ROUBO A TRANSEUNTE', 'ROUBO DE VEÍCULO', 'ROUBO EM RESIDÊNCIA', 'ESTUPRO', 'TRÁFICO DE DROGAS')
+            self.data.append(db_data)
 
-def treat_extracted_data(filtered_crimes):
-    city_data = []
+        print(self.data)
 
-    for key, value in filtered_crimes.items():
-        type_crime = {}
-        type_crime['crime_nature'] = key
-        type_crime['quantity'] = int(value)
-
-        city_data.append(type_crime)
-
-    return city_data
-    
-
-def get_datas_from_excel(name):
-    df_excel = pd.read_excel(f'./data/2020/{name}.xlsx', usecols=[i for i in range(1, 3)], index_col=0, skiprows=7, skipfooter=3)
-    df_excel = df_excel.rename(columns={'Unnamed: 2': 'Total'}, inplace=False)
-
-    crimes = dict(zip([str(crime) for crime in df_excel.index], [str(value[0]) for value in df_excel.values]))
-
-    # Filter dictionary by keeping elements whose keys are in crime_nature list 
-    filtered_crimes = dict(filter(lambda elem : elem[0] in crimes_nature, crimes.items()))
-
-    city_data = treat_extracted_data(filtered_crimes)
-    
-    return city_data
-
-
-def load_data(names_of_cities):
-    for city_name in names_of_cities:
-        crimes_data = {}
-        city_data = get_datas_from_excel(city_name)
-        crimes_data[city_name] = city_data
-
-        cities_data.append(crimes_data)
-
-    return cities_data
+            # TODO: Salvar no Banco

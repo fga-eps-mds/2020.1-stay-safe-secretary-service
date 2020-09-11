530a426ef4e5a605f48a903137d7ce743ccab9d3
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-09-10 21:49:25 -0300

    Set up DfPipeline on settings
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>

--- src/crawlers/df/df/settings.py
+++ src/crawlers/df/df/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'df.pipelines.DfPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'df.pipelines.DfPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html

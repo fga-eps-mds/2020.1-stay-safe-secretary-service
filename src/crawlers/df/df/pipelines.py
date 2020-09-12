3652915fd59be903aed662114b6d383ceb427d52
Lucas Boaventura <lucasxboaventura@hotmail.com>
2020-09-11 21:32:13 -0300

    Add connection and insert at database
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>

--- src/crawlers/df/df/pipelines.py
+++ src/crawlers/df/df/pipelines.py
@@ -1,38 +1,44 @@
 from itemadapter import ItemAdapter
 from utils.treat_dataDF import load_data
 import datetime
-
-# db_data = {
-#     "_id": 1,
-#     "capture_data": "04/08/2020",
-#     "period": {
-#         "start": "01/2020",
-#         "end": "07/2020"
-#     },
-#     "cities": []
-# }
+import pymongo
+from scrapy.utils.project import get_project_settings
 
 class DfPipeline:
-    data = []
+    collection_name = "DF"
+
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE')
+        )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
 
     def process_item(self, item, spider):
         return item
 
-
     def close_spider(self, spider):
         for year in spider.data['years']:
             db_data = {}
-            db_data["cities"] = load_data(spider.data['cities'], year)
+
+            date = datetime.datetime.now()
+            db_data['capture_data'] = f'{date.strftime("%d")}/{date.strftime("%m")}/{date.strftime("%Y")}'
+            
             db_data['period'] = {
                 'year': year
             }
 
-            date = datetime.datetime.now()
+            db_data["cities"] = load_data(spider.data['cities'], year)
 
-            db_data['capture_data'] = f'{date.strftime("%d")}/{date.strftime("%m")}/{date.strftime("%Y")}'
+            self.db[self.collection_name].insert_one(db_data)
 
-            self.data.append(db_data)
+        self.client.close()
 
-        print(self.data)
-
-            # TODO: Salvar no Banco

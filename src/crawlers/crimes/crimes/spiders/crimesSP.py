32eddfa6d4ff64d8a8258db060f664bd6bbbac77
Herick Portugues <herick.portugues@gmail.com>
2020-09-16 23:23:01 -0300

    Extract crime statistics from all cities in SP.
    Co-authored-by: Brenda Santos <brendasantosv.bs@gmail.com>

--- src/crawlers/crimes/crimes/spiders/crimesSP.py
+++ src/crawlers/crimes/crimes/spiders/crimesSP.py
@@ -1,5 +1,4 @@
 import scrapy
-import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -15,24 +14,45 @@ class CrimesSP(scrapy.Spider):
         self.driver = webdriver.Firefox()
 
     def parse(self, response):
-        cities_list = []
         self.driver.get(response.url)
-        table = self.driver.find_element_by_xpath('//*[@id="conteudo_ddlMunicipios"]')
-        table_items = table.find_elements_by_tag_name('option')
         
-        for i in range (1, len(table_items) - 1):
+        cities_list = response.xpath('//*[@id="conteudo_ddlMunicipios"]//option')
+
+        self.driver.find_element_by_xpath('//*[@id="conteudo_btnMensal"]').click()
+
+        data = {}
+        for i in range (1, len(cities_list)-1):
             select_regions = Select(WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="conteudo_ddlRegioes"]'))))
             select_regions.select_by_value('0')
             
             select_cities = Select(WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="conteudo_ddlMunicipios"]'))))
             select_cities.select_by_value(str(i))
-
-            time.sleep(2)
             
             source = self.driver.page_source
-            sel = Selector(text=source) 
-            data = sel.xpath('//*[@id="conteudo_lkMunicipio"]').get()
-            
-            import ipdb; ipdb.set_trace()
+            selector = Selector(text=source)
+
+            city_name = selector.xpath('//*[@id="conteudo_lkMunicipio"]/text()').get().replace(' | ', '')
+
+            city_data = []
+            for j in range(0, 3):
+                city_year_data = {}
+
+                table_crimes_year = selector.xpath(f'//*[@id="conteudo_repAnos_gridDados_{str(j)}"]/tbody//tr')
+
+                annual_crimes_registers = []
+                for k, crime in enumerate(table_crimes_year):
+                    if k > 0:
+                        crimes_data = {}
+                        crimes_data['crime_nature'] = crime.xpath('./td[1]/text()').get()
+                        crimes_data['quantity'] = int(crime.xpath(f'./td[14]/text()').get().replace('.', ''))
+
+                        annual_crimes_registers.append(crimes_data)
+
+                city_year_data['year'] = int(selector.xpath(f'//*[@id="conteudo_repAnos_lbAno_{str(j)}"]/text()').get())
+                city_year_data['crimes_data'] = annual_crimes_registers
+
+                city_data.append(city_year_data)
+
+            data[city_name] = city_data
         
         self.driver.close()

5e39545022c1cbdde1249da87acff93c53539b94
Herick Portugues <herick.portugues@gmail.com>
2020-10-12 20:33:13 -0300

    Update spider crimes_df to work with DfPipeline and DfItem.
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

--- src/crawlers/crimes/crimes/spiders/crimesDF.py
+++ src/crawlers/crimes/crimes/spiders/crimesDF.py
@@ -3,7 +3,9 @@ Import scrapy and utils functions.
 """
 import urllib.request
 import scrapy
+
 from utils.handle_folders import create_folder
+from ..items import DfItem
 
 class CrimesDF(scrapy.Spider):
     """
@@ -13,43 +15,44 @@ class CrimesDF(scrapy.Spider):
     allowed_domains = "http://www.ssp.df.gov.br/"
     start_urls = ["http://www.ssp.df.gov.br/dados-por-regiao-administrativa/"]
 
-    data = {
-        'years': [],
-        'cities': [],
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'crimes.pipelines.DfPipeline': 300,
+        }
     }
 
     def parse(self, response):
         """
         Function to get all the excel files and save.
         """
-        crime_table = response.xpath(
-            '/html/body/div[8]/div/div/div/div[2]/table[2]/tbody//tr')
+        data_table = response.xpath('//*[@id="conteudo"]/table[2]/tbody//tr')
 
         create_folder('data')
 
-        for i, city in enumerate(crime_table):
+        for i, city in enumerate(data_table):
             if i > 1:
                 city_name = city.xpath(
                     './td[1]/strong/text()').get().replace('/', " ")
-                self.data['cities'].append(city_name)
 
-                # Get a list of all tds of the row (collumns)
-                td_list = city.xpath('.//td')
+                # Get a list of all columns of the city row
+                columns_table = city.xpath('.//td')
 
                 # Initial year of the data
                 year = 2018
 
-                for j in range(16, len(td_list) + 1, 2):
-                    data_url = city.xpath(f'./td[{j}]/a/@href').get()
-
-                    if year not in self.data['years']:
-                        create_folder(f'data/{str(year)}')
-                        self.data['years'].append(year)
+                # Download the annual excel table of a city
+                # and process the item to save on database.
+                for excel_table in range(16, len(columns_table) + 1, 2):
+                    data_url = city.xpath(f'./td[{excel_table}]/a/@href').get()
 
                     if data_url is not None:
                         # Download and save the excel table
                         urllib.request.urlretrieve(
                             str(data_url),
-                            f'./data/{str(year)}/{city_name}.xlsx')
+                            f'./data/{city_name}.xlsx')
+
+                        # Create a new DfItem and process it for each annual table of a city
+                        spiderItem = DfItem(year=year, city=city_name)
+                        yield spiderItem
 
                     year += 1

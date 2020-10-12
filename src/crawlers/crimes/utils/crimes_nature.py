89a13685fee5b215e8af971b3544e7f531ae948d
Herick Portugues <herick.portugues@gmail.com>
2020-10-12 20:43:51 -0300

    Create a file to save crimes_nature constants and move crimes_nature_df to it.
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

---
+++ src/crawlers/crimes/utils/crimes_nature.py
@@ -0,0 +1,14 @@
+"""
+The keys are the names in the original file
+and the values are the names that will be saved in the database. 
+"""
+
+crimes_nature_df = {
+    'LATROCÍNIO': "Latrocínio",
+    'ROUBO A TRANSEUNTE': 'Roubo a Transeunte',
+    'ROUBO DE VEÍCULO': 'Roubo de Veículo',
+    'ROUBO EM RESIDÊNCIA': 'Roubo de Residência',
+    'FURTO EM VEÍCULO': 'Furto de Veículo',
+    'FURTO A TRANSEUNTE': 'Furto a Transeunte',
+    'ESTUPRO': 'Estupro',
+}

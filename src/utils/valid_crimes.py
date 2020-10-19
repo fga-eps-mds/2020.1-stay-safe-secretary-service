47f8ed79aea50a66721326dbc10bdd13e69e7080
Herick Portugues <herick.portugues@gmail.com>
2020-10-19 20:14:37 -0300

    Create utils valid_crimes with the valid crimes of each state.
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

---
+++ src/utils/valid_crimes.py
@@ -0,0 +1,18 @@
+valid_crimes_df = [
+    'Latrocínio',
+    'Roubo a Transeunte',
+    'Roubo de Veículo',
+    'Roubo de Residência',
+    'Furto de Veículo',
+    'Estupro',
+    'Furto a Transeunte',
+]
+
+valid_crimes_sp = [
+    'Latrocínio',
+    'Estupro',
+    'Outros Roubos',
+    'Roubo de Veículo',
+    'Outros Furtos',
+    'Furto de Veículo',
+]

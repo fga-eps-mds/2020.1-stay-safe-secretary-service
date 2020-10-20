35360208b74d6cc1c1721e224850f9654975bb08
Herick Portugues <herick.portugues@gmail.com>
2020-10-20 20:50:00 -0300

    Create get_crimes_per_capita function.
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

--- src/utils/filters/crimes_filters.py
+++ src/utils/filters/crimes_filters.py
@@ -1,3 +1,5 @@
+import json
+
 def get_cumulative_amounts_of_crimes(data, initial_month, final_month):
     new_data = data[0]
     new_data['period'] = f'{initial_month}-{final_month}'
@@ -9,3 +11,18 @@ def get_cumulative_amounts_of_crimes(data, initial_month, final_month):
                     new_data['cities'][j]['crimes'][k]['quantity'] += crime['quantity']
 
     return new_data
+
+def get_crimes_per_capita(data, secretary):
+    populationData = open('static_data/populationData.json',)
+    populationData = json.load(populationData)
+
+    populations = list(filter(lambda state: state['state'] == secretary, populationData['populations']))
+
+    for _data in data:
+        for city in _data['cities']:
+            for crime in city['crimes']:
+                city_data = list(filter(lambda x: x['name'] == city['name'], populations[0]['cities']))
+                if city_data != []:
+                    crime['quantity'] = crime['quantity'] / (city_data[0]['population']/10)
+
+    return data

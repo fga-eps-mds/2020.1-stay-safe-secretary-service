8cd25f0af6916b838df10aa5c60549ece54cf3cf
Herick Portugues <herick.portugues@gmail.com>
2020-10-21 19:25:27 -0300

    Calculate range of crimes per 100k inhabitants.
    Co-authored-by: Daniel Primo <danielprimo9@gmail.com>

--- src/utils/amount_crimes.py
+++ src/utils/amount_crimes.py
@@ -1,5 +1,7 @@
 import json
 
+from utils.constants import ANNUAL_CRIMES_RANGE
+
 def get_cumulative_amounts_of_crimes(data, initial_month, final_month):
     new_data = data[0]
     new_data['period'] = f'{initial_month}-{final_month}'
@@ -12,17 +14,42 @@ def get_cumulative_amounts_of_crimes(data, initial_month, final_month):
 
     return new_data
 
-def get_crimes_per_capita(data, secretary):
+def get_crimes_per_capita(data, secretary, months):
     populationData = open('static_data/populationData.json',)
     populationData = json.load(populationData)
 
-    populations = list(filter(lambda state: state['state'] == secretary, populationData['populations']))
+    populations = list(filter(lambda state: state['state'] == secretary,
+        populationData['populations']))
 
     for _data in data:
         for city in _data['cities']:
             for crime in city['crimes']:
-                city_data = list(filter(lambda x: x['name'] == city['name'], populations[0]['cities']))
+                city_data = list(filter(
+                    lambda x: x['name'] == city['name'], populations[0]['cities']))
                 if city_data != []:
-                    crime['quantity'] = crime['quantity'] / (city_data[0]['population']/10)
+                    crime['quantity'] = (crime['quantity'] / city_data[0]['population']) * 100
+                    crime['classification'] = get_range(crime, secretary, months)
 
     return data
+
+def get_range(crime, secretary, months):
+    annual_state_crimes_range = list(
+        filter(lambda state: state['state'] == secretary, ANNUAL_CRIMES_RANGE))[0]
+
+    if crime['quantity'] <= months / 12 * \
+            annual_state_crimes_range['crimes_range'][crime['nature']][0]:
+        return 1
+    elif crime['quantity'] <= months / 12 * \
+            annual_state_crimes_range['crimes_range'][crime['nature']][1]:
+        return 2
+    elif crime['quantity'] <= months / 12 * \
+            annual_state_crimes_range['crimes_range'][crime['nature']][2]:
+        return 3
+    elif crime['quantity'] <= months / 12 * \
+            annual_state_crimes_range['crimes_range'][crime['nature']][3]:
+        return 4
+    elif crime['quantity'] <= months / 12 * \
+            annual_state_crimes_range['crimes_range'][crime['nature']][4]:
+        return 5
+    else:
+        return 6
